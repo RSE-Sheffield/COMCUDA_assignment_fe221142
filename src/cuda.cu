6b656b3430b159b6d99701bef1f22f06d0e5f586
Robert Chisholm <r.chisholm@sheffield.ac.uk>
2026-01-27 15:33:09 +0000

    Initial handout code.
    Based input/output testing was carried out, which caught several bugs.

---
+++ src/cuda.cu
@@ -0,0 +1,13 @@
+#include "cuda.cuh"
+
+uint64_t cuda_countGliders(const unsigned char *cells, const size_t width, const size_t height) {
+    return 0;  // todo!
+}
+
+size_t cuda_histogram(const int *numbers, size_t length, int bin_width, int *output) {
+    return 0;  // todo!
+}
+
+void cuda_emboss(const unsigned char *pixels, const size_t width, const size_t height, unsigned char *output) {
+    // todo!
+}
---
+++ src/openmp.c
@@ -0,0 +1,14 @@
+#include "openmp.h"
+#include <omp.h>
+
+uint64_t openmp_countGliders(const unsigned char *cells, const size_t width, const size_t height) {
+    return 0;  // todo!
+}
+
+size_t openmp_histogram(const int *numbers, size_t length, int bin_width, int *output) {
+    return 0;  // todo!
+}
+
+void openmp_emboss(const unsigned char *pixels, const size_t width, const size_t height, unsigned char* output) {
+    // todo!
+}
